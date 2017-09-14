afccd8cd737cea7eb7d358b436160581accc9a9e
= <angelica@tokyoplay.com>
2017-09-14 11:32:41 -0500

    Initial Commit

---
+++ SimpleARG/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleARG
+//
+//  Created by Angelica Barreda on 14/09/17.
+//  Copyright © 2017 Angelica Barreda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
